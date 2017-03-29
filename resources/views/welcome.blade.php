bed233fb60aeb9226603fe5f3e65d7d5695e4a93
Mario Macuacua <mariocarlitosmacuacua@gmail.com>
2017-03-29 23:42:27 +0200

    make it better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Pagina Inicial
                 </div>
 
                 <div class="links">
